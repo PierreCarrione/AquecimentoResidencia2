475f721845e7c58287c48eb4d638f3e08eda69c0
PierreCarrione <50272580+PierreCarrione@users.noreply.github.com>
2022-11-23 09:57:45 -0300

    Update Principal.cs

--- Numero4/Principal.cs
+++ Numero4/Principal.cs
@@ -5,10 +5,13 @@ public class Principal
     public static void Main(String[] args)
     {
         Pessoa pierre = new Pessoa("Pierre");
+        Pessoa pedro = new Pessoa("pedro");
         CertidaoNascimento cert = new CertidaoNascimento(DateTime.Now, pierre);
-        CertidaoNascimento certi = new CertidaoNascimento(DateTime.Now, pierre);
+        CertidaoNascimento certi = new CertidaoNascimento(DateTime.Now, pedro);
         pierre.Certidao = cert;
+        pedro.Certidao = certi;
         pierre.showDados();
+        pedro.showDados();
         pierre.Certidao = certi;
     }
 }
